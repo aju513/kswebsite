d0f8bb42fb69beb79cef0011a04c4e634cbf0000
aju513 <99ajumrzn@gmail.com>
2024-02-29 13:30:57 +0545

    svg

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import YourSvgComponent from "../components/SvgComponent";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
         <div className="bg-green-600 h-[45rem] w-[34rem] lg:h-[40rem] lg:w-[50rem] rounded-[10rem] absolute -bottom-[20%] left-[70%] animation rotation-45-deg"></div>
         <div className="bg-gradient-to-br from-red-500 to-white h-[15rem] w-[40rem] rounded-[10rem] absolute top-[34%] left-[76%] opacity-90 animation rotation-neg-45-deg"></div>
       </div>
+      <YourSvgComponent />
     </>
   );
 };
